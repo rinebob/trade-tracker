89b431b873f74f1d7d91c368db395e40750085d5
rinebob <rinebob111185@gmail.com>
2024-08-27 16:43:52 -0700

    [app] 1 - Initial commit - Includes Angular and Firebase and initial deployment

---
+++ .firebase/trade-tracker-f3f2c/hosting/assets/secrets/fb-config.ts
@@ -0,0 +1,10 @@
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCAV3aGD0neDV_pEa-IIjQsnACbSpNUXaY",
+    authDomain: "trade-tracker-f3f2c.firebaseapp.com",
+    projectId: "trade-tracker-f3f2c",
+    storageBucket: "trade-tracker-f3f2c.appspot.com",
+    messagingSenderId: "450176747281",
+    appId: "1:450176747281:web:969cbd67fdc379ddbc2eb0"
+  };
